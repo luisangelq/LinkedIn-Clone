10924412e7074321dfedbb46cf5c51589cb0a311
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2022-08-17 02:15:26 -0500

    Right side card Finished

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -12,10 +12,52 @@ const RightSide = () => {
         <FeedList>
           <li>
             <a>
-              
+              <img src="/images/hash-icon.svg" alt="#" />
+
+              <InfoFeed>
+                <span>#LinkedIn</span>
+                <button>
+                  <img src="/images/plus-icon.svg" alt="Add" />
+                  Follow
+                </button>
+              </InfoFeed>
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <img src="/images/hash-icon.svg" alt="#" />
+
+              <InfoFeed>
+                <span>#Video</span>
+                <button>
+                  <img src="/images/plus-icon.svg" alt="Add" />
+                  Follow
+                </button>
+              </InfoFeed>
+            </a>
+          </li>
+          <li>
+            <a>
+              <img src="/images/hash-icon.svg" alt="#" />
+
+              <InfoFeed>
+                <span>#Video</span>
+                <button>
+                  <img src="/images/plus-icon.svg" alt="Add" />
+                  Follow
+                </button>
+              </InfoFeed>
             </a>
           </li>
         </FeedList>
+
+        <Footer>
+          <a>
+            <span>View all recommendations</span>
+            <img src="/images/right-icon.svg" alt="Arrow" />
+          </a>
+        </Footer>
       </FollowCard>
     </Container>
   );
@@ -31,7 +73,7 @@ const FollowCard = styled.div`
   border-radius: 0.5rem;
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
   border: 1px solid #e6e6e6;
-  padding: 1rem .8rem;
+  padding: 1rem 0.8rem;
 `;
 
 const Title = styled.div`
@@ -53,6 +95,82 @@ const Title = styled.div`
   }
 `;
 
-const FeedList = styled.ul``;
+const FeedList = styled.ul`
+  list-style: none;
+  margin-top: 1.5rem;
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+
+    a {
+      display: flex;
+      gap: 1rem;
+
+      img {
+        width: 48px;
+        height: 48px;
+      }
+    }
+  }
+`;
+
+const InfoFeed = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  span {
+    font-size: 14px;
+    font-weight: 600;
+    color: rgba(0, 0, 0, 0.8);
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+    font-size: 15px;
+    font-weight: 600;
+    color: rgba(0, 0, 0, 0.5);
+    border: 2px solid transparent;
+    border-radius: 5rem;
+    padding: 0.3rem 0.8rem;
+    margin: 2px;
+    outline: 1px solid rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    img {
+      width: 16px !important;
+      height: 16px !important;
+    }
+
+    &:hover {
+      background-color: #ebebeb;
+      outline: 2px solid rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
+
+const Footer = styled.div`
+  margin-top: 1.5rem;
+  padding: .5rem .5rem;
+  border-radius: 4px;
+  transition: all 0.2s ease-in-out;
+  a {
+  display: flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.5);
+  font-size: 13px;
+  font-weight: 600;
+  gap: .2rem;
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`
 
 export default RightSide;
